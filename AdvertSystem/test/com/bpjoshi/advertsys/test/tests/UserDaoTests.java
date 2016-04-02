36e4ce1e75e62cd99b17e94bf72328a2573fbdbe
bpjoshi <bpjoshi@bpjoshi.com>
2016-04-02 21:13:20 +0530

    UserDao JUnit Test Cases Added

--- AdvertSystem/test/com/bpjoshi/advertsys/test/tests/UserDaoTests.java
+++ AdvertSystem/test/com/bpjoshi/advertsys/test/tests/UserDaoTests.java
@@ -2,11 +2,20 @@ package com.bpjoshi.advertsys.test.tests;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.bpjoshi.advertsys.dao.UserDao;
+import com.bpjoshi.advertsys.model.User;
 
 @ActiveProfiles("dev")
 @ContextConfiguration(locations={"classpath:com/bpjoshi/advertsys/config/dao-context.xml",
@@ -14,8 +23,31 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 		, "classpath:com/bpjoshi/advertsys/test/config/datasource.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserDaoTests {
+	
+	@Autowired
+	private UserDao userDao;
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	public void init(){
+		JdbcTemplate jdbc= new JdbcTemplate(dataSource);
+		jdbc.execute("delete from authorities");
+		jdbc.execute("delete from users");
+	}
+	
 	@Test
-	public void testCreateUser(){
-		assertEquals("Dummy Test", 1, 1);
+	public void testCreateAccount(){
+		
+		User user= new User("rohit", "rohit@palariya.com", "password", true, "ROLE_USER");
+		assertTrue("Admin Account Created", userDao.createAccount(user));
+		
+		List<User> userList= userDao.getAllUsers();
+		assertEquals("Number of User should be 1",1, userList.size());
+		
+		assertTrue("This user should exist", userDao.exists(user.getUsername()));
+		
+		assertEquals("The users must be euqal", user, userList.get(0));
 	}
 }
